af712b9e803b0283d9070a8e8c270416a8f1a7bc
Ian Christiansen <ianc.songdog@gmail.com>
2022-01-25 11:56:29 -0800

    completed assignment

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,18 @@
+import {
+  fetchFilms,
+  getFilms,
+  fetchCharacters,
+  totoroCharacters,
+} from './services/promise-me';
+import { useEffect } from 'react';
+
 export default function App() {
+  useEffect(() => {
+    fetchFilms();
+    getFilms();
+    fetchCharacters();
+    totoroCharacters();
+  });
+
   return <h1>Hello World</h1>;
 }
--- src/services/promise-me.js
+++ src/services/promise-me.js
@@ -1,35 +1,39 @@
-/**
- * TODO:
- */
+export const fetchFilms = () => {
+  fetch('https://ghibliapi.herokuapp.com/films')
+    .then((data) => data.json())
+    .then((response) => {
+      console.log('then films', response);
+    });
+};
 
-/**
- * TODO:
- */
+export async function getFilms() {
+  const resp = await fetch('https://ghibliapi.herokuapp.com/films');
+  const data = await resp.json();
+  console.log('async films', data);
+}
 
-/**
- * TODO:
- */
+export const fetchCharacters = () => {
+  fetch('https://ghibliapi.herokuapp.com/people')
+    .then((data) => data.json())
+    .then((response) => {
+      const characterNames = response.map((characterName) => ({
+        name: characterName.name,
+      }));
+      console.log('character names', characterNames);
+    });
+};
 
-/**
- * TODO:
- */
-
-/**
- * TODO:
- */
-
-/**
- * TODO:
- */
-
-/**
- * TODO:
- */
-
-/**
- * TODO:
- */
-
-/**
- * TODO:
- */
+export async function totoroCharacters() {
+  const resp = await fetch('https://ghibliapi.herokuapp.com/people');
+  const data = await resp.json();
+  console.log('characters', data);
+  const allCharacters = data.map((character) => ({
+    characters: character.name,
+  }));
+  const totoroCharacters = allCharacters.filter(
+    (allCharacters) =>
+      allCharacters.films ==
+      'https://ghibliapi.herokuapp.com/films/58611129-2dbc-4a81-a72f-77ddfc1b1b49'
+  );
+  console.log('crappity crap', totoroCharacters);
+}
